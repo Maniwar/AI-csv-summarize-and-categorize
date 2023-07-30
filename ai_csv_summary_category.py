cf7c025d5cbc1b176702350085aa2482d85eddb9
Maniwar <mberenji@gmail.com>
2023-07-30 12:13:28 -0700

    Update ai_csv_summary_category.py

--- ai_csv_summary_category.py
+++ ai_csv_summary_category.py
@@ -25,6 +25,11 @@ import re
 import string
 import unicodedata
 
+# Specify download directory for NLTK data
+nltk.download('stopwords', download_dir='/home/appuser/nltk_data')
+nltk.download('vader_lexicon', download_dir='/home/appuser/nltk_data')
+nltk.download('punkt', download_dir='/home/appuser/nltk_data', quiet=True)  # Add 'quiet=True' to suppress NLTK download messages
+nltk.data.path.append('/home/appuser/nltk_data')
 
 # Initialize BERT model
 @st.cache_resource
