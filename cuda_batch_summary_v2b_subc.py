2fa89adf3b8962602474c47bc24ec2f63753a633
Maniwar <mberenji@gmail.com>
2023-09-29 11:33:29 -0700

    Update cuda_batch_summary_v2b_subc.py

--- cuda_batch_summary_v2b_subc.py
+++ cuda_batch_summary_v2b_subc.py
@@ -25,9 +25,11 @@ import re
 import string
 import unicodedata
 
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('vader_lexicon')
+# Specify download directory for NLTK data
+nltk.download('stopwords', download_dir='/home/appuser/nltk_data')
+nltk.download('vader_lexicon', download_dir='/home/appuser/nltk_data')
+nltk.download('punkt', download_dir='/home/appuser/nltk_data', quiet=True)  # Add 'quiet=True' to suppress NLTK download messages
+nltk.data.path.append('/home/appuser/nltk_data')
 
 # Initialize BERT model
 @st.cache_resource
